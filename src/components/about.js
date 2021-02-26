33b8ade05c3f0870ee0bcd04da8f6b7fde43a1cf
Brandie Burditt <brandieburditt@gmail.com>
2021-02-26 14:41:24 -0500

    refactor: Update css padding in about section

--- src/components/about.js
+++ src/components/about.js
@@ -1,18 +1,9 @@
 import React from 'react';
-import Img from 'gatsby-image';
 
 const Header = ({ data }) => (
   <div className="about section" id="About">
     <div className="container">
       <div className="about-main row">
-        {/* <div className="left col-md-5 col-lg-4 mb-3">
-          <Img
-            fluid={data.photo.fluid}
-            objectFit="cover"
-            objectPosition="top center"
-          />
-        </div> */}
-        {/* <div className="left col-md-7 col-lg-8"> */}
         <div className="about-details">
           <span className="name">My Name is {data.name}.</span>
           <h2 className="sub-position">
@@ -60,7 +51,6 @@ const Header = ({ data }) => (
             </ul>
           </div>
         </div>
-        {/* </div> */}
       </div>
     </div>
   </div>
